7067c3574af2620167311e616669622e161814ec
Nadeen Radwan <nadeenradwan0@gmail.com>
2023-02-05 21:23:08 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
+
+
+}
