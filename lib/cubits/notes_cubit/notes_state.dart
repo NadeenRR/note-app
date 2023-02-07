c72c8709637fd15cfc3bfad2a6d034fbb790454d
Nadeen Radwan <65049320+NadeenRR@users.noreply.github.com>
2023-02-07 22:34:04 +0200

    update notes state

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -4,25 +4,4 @@ part of 'notes_cubit.dart';
 abstract class NotesState {}
 
 class NotesInitial extends NotesState {}
-<<<<<<< HEAD
 class NotesSuccess extends NotesState {}
-
-
-
-
-=======
-
-class AddNoteLoading extends NotesState {}
-
-class AddNoteSuccess extends NotesState {
-  final List<NoteModel> notes;
-
-  AddNoteSuccess(this.notes);
-}
-
-class AddNoteFailure extends NotesState {
-  final String errMeaage;
-
-  AddNoteFailure(this.errMeaage);
-}
->>>>>>> 556427a44580c72cfd9dadea0090979c56630f78
